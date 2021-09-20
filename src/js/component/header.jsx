c96dd4dd7ea0edf91b9434e0fa0bed643fb6d7fe
jenkins96 <adrianjnkns@gmail.com>
2021-09-20 05:46:46 +0000

    Created navbar

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+///workspace/25Watts-Project/src/img/top-logo.svg
 
 //create your first component
 const Header = () => {
-	return <h1>Hey</h1>;
+	return (
+		<nav>
+			<div>
+				<a>
+					<img src="../../img/top.logo.svg" alt="MM" />
+				</a>
+			</div>
+			<div>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Products</li>
+					<li>Services</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
