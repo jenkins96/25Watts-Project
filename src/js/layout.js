ab443b6a0d7bf4beb392ce8704ca524ad46c418d
jenkins96 <adrianjnkns@gmail.com>
2021-09-20 05:47:33 +0000

    Changed layout due to file renamed

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,5 +1,10 @@
 import React, { Fragment } from "react";
-import Home from "./component/home.jsx";
+import { Home } from "./views/home";
+import { About } from "./views/about";
+import { Products } from "./views/products";
+import { Services } from "./views/services";
+import { Contact } from "./views/contact";
+//import Home from "./component/home.jsx";
 import Header from "./component/header.jsx";
 import Footer from "./component/footer.jsx";
 
@@ -13,6 +18,10 @@ const Layout = () => {
 		<Fragment>
 			<Header />
 			<Home />
+			<About />
+			<Products />
+			<Services />
+			<Contact />
 			<Footer />
 		</Fragment>
 	);
