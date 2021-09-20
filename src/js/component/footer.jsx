2c97b66e68b9e131f6f4092cf7b1991288649898
jenkins96 <adrianjnkns@gmail.com>
2021-09-20 04:09:11 +0000

    Created header and footer components

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//create your first component
+const Footer = () => {
+	return (
+	<h1>Hey</h1>
+	);
+};
+
+export default Footer;
---
+++ src/js/component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//create your first component
+const Header = () => {
+	return (
+        <h1>Hey</h1>	
+	);
+};
+
+export default Header;
