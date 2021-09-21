e145ad753e3ea1eefc8d725ff26a10572e76bafb
jenkins96 <adrianjnkns@gmail.com>
2021-09-21 03:46:30 +0000

    Still working

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -4,27 +4,29 @@ import "../../styles/about.scss";
 //  font-family: 'Poppins', sans-serif;
 
 export const About = () => (
-	<div className="aboutBg container-fluid col-md-6 offset-2">
-		<div className="mainContentAbout">
-			<h2 className="mainTitleAbout">
-				We Help Businesses{" "}
-				<span className="colorTitle">Grow and Innovate</span>
-			</h2>
-			<h2 className="mainTitleAbout colorTitle">Grow and Innovate</h2>
-			<p className="mainPAbout">
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-				diam nonumy eirmod tempor invidunt ut labore et dolore magna
-				aliquyam erat, sed diam voluptua. At vero eos et accusam et
-				justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-				takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-				dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-				eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-				sed diam voluptua. At vero eos et accusam et justo duo dolores
-				et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-				est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-				consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-				invidunt ut
-			</p>
+	<div className="aboutBg container-fluid">
+		<div className="row">
+			<div className="col-md-2 vl"></div>
+			<div className="mainContentAbout col-md-8">
+				<h2 className="mainTitleAbout">
+					We Help Businesses{" "}
+					<span className="colorTitle">Grow and Innovate</span>
+				</h2>
+				<p className="mainPAbout">
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+					diam nonumy eirmod tempor invidunt ut labore et dolore magna
+					aliquyam erat, sed diam voluptua. At vero eos et accusam et
+					justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+					sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+					ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+					nonumy eirmod tempor invidunt ut labore et dolore magna
+					aliquyam erat, sed diam voluptua. At vero eos et accusam et
+					justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+					sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+					ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+					nonumy eirmod tempor invidunt ut
+				</p>
+			</div>
 		</div>
 	</div>
 );
