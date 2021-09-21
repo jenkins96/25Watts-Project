1aa23a8a687672ad000edb0d201cb83d58e2288e
jenkins96 <adrianjnkns@gmail.com>
2021-09-21 16:54:35 +0000

    Working in services views

--- src/js/views/services.js
+++ src/js/views/services.js
@@ -8,26 +8,5 @@ export const Services = () => (
 			<div className="col-md-6 bgServices1"></div>
 			<div className="col-md-6 bgServices2"></div>
 		</div>
-		<div className="btn-group">
-			<button
-				type="button"
-				className="btn btn-secondary dropdown-toggle"
-				data-toggle="dropdown"
-				aria-haspopup="true"
-				aria-expanded="false">
-				This dropdowns menu is right-aligned
-			</button>
-			<div className="dropdown-menu dropdown-menu-right">
-				<button className="dropdown-item" type="button">
-					Action
-				</button>
-				<button className="dropdown-item" type="button">
-					Another action
-				</button>
-				<button className="dropdown-item" type="button">
-					Something else here
-				</button>
-			</div>
-		</div>
 	</div>
 );
