23a4973b82678058e7da8234bee315007641b0b5
jenkins96 <adrianjnkns@gmail.com>
2021-09-21 16:22:43 +0000

    working

--- src/js/views/services.js
+++ src/js/views/services.js
@@ -1,15 +1,33 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
-//import "../../styles/dropDown.scss";
+import "../../styles/services.scss";
 
 export const Services = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container-fluid">
+		<div className="row">
+			<div className="col-md-6 bgServices1"></div>
+			<div className="col-md-6 bgServices2"></div>
+		</div>
+		<div className="btn-group">
+			<button
+				type="button"
+				className="btn btn-secondary dropdown-toggle"
+				data-toggle="dropdown"
+				aria-haspopup="true"
+				aria-expanded="false">
+				This dropdowns menu is right-aligned
+			</button>
+			<div className="dropdown-menu dropdown-menu-right">
+				<button className="dropdown-item" type="button">
+					Action
+				</button>
+				<button className="dropdown-item" type="button">
+					Another action
+				</button>
+				<button className="dropdown-item" type="button">
+					Something else here
+				</button>
+			</div>
+		</div>
 	</div>
 );
