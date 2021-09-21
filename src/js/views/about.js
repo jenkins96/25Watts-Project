ab48d0b00e6c624af7abcf4d6bcc724dc6e9e115
jenkins96 <adrianjnkns@gmail.com>
2021-09-21 00:14:20 +0000

    Working in about page

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -6,8 +6,25 @@ import "../../styles/about.scss";
 export const About = () => (
 	<div className="aboutBg container-fluid col-md-6 offset-2">
 		<div className="mainContentAbout">
-			<h2 className="mainTitleAbout">We Help Businesses</h2>
+			<h2 className="mainTitleAbout">
+				We Help Businesses{" "}
+				<span className="colorTitle">Grow and Innovate</span>
+			</h2>
 			<h2 className="mainTitleAbout colorTitle">Grow and Innovate</h2>
+			<p className="mainPAbout">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+				diam nonumy eirmod tempor invidunt ut labore et dolore magna
+				aliquyam erat, sed diam voluptua. At vero eos et accusam et
+				justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+				takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+				dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+				eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+				sed diam voluptua. At vero eos et accusam et justo duo dolores
+				et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+				est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+				consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+				invidunt ut
+			</p>
 		</div>
 	</div>
 );
